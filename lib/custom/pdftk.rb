a299ce6c248181122d2d2e24b549f50380c10e0d
Joel Mahoney <joelmahoney@gmail.com>
2016-05-16 16:28:07 -0700

    Add S3 URL

--- lib/custom/pdftk.rb
+++ lib/custom/pdftk.rb
@@ -9,7 +9,7 @@ class Pdftk < BaseCustom
   end
 
   def source_url
-    "https://s3-us-west-2.amazonaws.com/noaapp/pdftk.tar.gz"
+    "https://s3.amazonaws.com/opencounter-misc/pdftk.tar.gz"
   end
 
   def used?
